8362599830c9f208bb10074e633dcc47c877ee64
Julian <julicogui@gmail.com>
2023-07-20 01:59:04 -0300

    fix: minor fix

--- src/app/components/base/Spinner/Spinner.tsx
+++ src/app/components/base/Spinner/Spinner.tsx
@@ -1,12 +1,10 @@
-import { CircularProgress } from "@mui/material"
-
 export const Spinner = () => (
     <div className="spinner-container">
       <div className="spinner">
-  <span className="spinner-inner-1"></span>
-  <span className="spinner-inner-2"></span>
-  <span className="spinner-inner-3"></span>
-</div>
+        <span className="spinner-inner-1"></span>
+        <span className="spinner-inner-2"></span>
+        <span className="spinner-inner-3"></span>
+      </div>
     </div>
   )
 
