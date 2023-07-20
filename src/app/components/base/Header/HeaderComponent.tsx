260f9da1b3a1e7b4f593553fabc96d4a46d337e5
Julian <julicogui@gmail.com>
2023-07-20 05:26:51 -0300

    minor fix

--- src/app/components/base/Header/HeaderComponent.tsx
+++ src/app/components/base/Header/HeaderComponent.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
-
-export const HeaderComponent = () => {
-  return (
+export const HeaderComponent = () => (
     <div className='header-container'>
         <h1 className='title'>Imgur</h1>
     </div>
   )
-}
+
--- src/app/components/base/Paginator/Paginator.tsx
+++ src/app/components/base/Paginator/Paginator.tsx
@@ -1,6 +1,5 @@
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import { BaseFilters } from '../../../models/Filters';
 import { Button } from '@mui/material';
 
 export const Paginator = ({ 
